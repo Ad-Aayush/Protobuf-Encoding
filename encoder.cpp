ed4248270b7245466343de8055c0a2c69d45bc61
Aayush Adlakha <aayushadlakha15@gmail.com>
2025-12-30 09:02:55 +0000

    Add support for strings and double

--- encoder.cpp
+++ encoder.cpp
@@ -52,18 +52,29 @@ std::string encodeI64(double num) {
   return enc;
 }
 
-std::optional<long long> decodeVarint(const std::string &str, int index = 0) {
+std::string encodeStr(const std::string &str) {
+  std::string enc;
+  int sz = str.size();
+  enc += intToBits(sz);
+  for (char c : str) {
+    enc += intToBits(static_cast<int>(c));
+  }
+  return enc;
+}
+
+std::pair<std::optional<long long>, int> decodeVarint(const std::string &str,
+                                                      int index = 0) {
   int sz = str.size();
   long long res = 0;
   long long pow128 = 1;
-  for (int i = index; i < sz; i += 8) {
-    bool last = str[i] == '0';
+  for (; index < sz; index += 8) {
+    bool last = str[index] == '0';
     long long partInt = 0;
     long long pow2 = 64;
-    if (i + 8 > sz) {
-      return std::nullopt;
+    if (index + 8 > sz) {
+      return {std::nullopt, index};
     }
-    for (int j = i + 1; j < i + 8; j++) {
+    for (int j = index + 1; j < index + 8; j++) {
       if (str[j] == '1') {
         partInt += pow2;
       }
@@ -75,7 +86,7 @@ std::optional<long long> decodeVarint(const std::string &str, int index = 0) {
       break;
     }
   }
-  return res;
+  return {res, index};
 }
 
 std::optional<double> decodeI64(const std::string &str, int index = 0) {
@@ -96,12 +107,46 @@ std::optional<double> decodeI64(const std::string &str, int index = 0) {
   return res;
 }
 
+std::pair<std::optional<std::string>, int> decodeStr(const std::string &str,
+                                                     int index = 0) {
+  int sz = str.size();
+  std::string res;
+  auto [maybeRecSz, newIndex] = decodeVarint(str, index);
+  if (!maybeRecSz.has_value()) {
+    return {std::nullopt, index};
+  }
+  int recSz = *maybeRecSz;
+  std::cout << recSz << "\n";
+  index = newIndex;
+  res.reserve(recSz);
+  while (res.size() <= recSz) {
+    if (index + 8 > sz) {
+      return {std::nullopt, index};
+    }
+    int charInt = 0;
+    int pow2 = 128;
+    for (int j = index; j < index + 8; j++) {
+      if (str[j] == '1') {
+        charInt += pow2;
+      }
+      pow2 /= 2;
+    }
+    res += static_cast<char>(charInt);
+    index += 8;
+  }
+  return {res, index};
+}
+
 int main() {
   std::string enc = encodeVarint(150);
   std::cout << enc << "\n";
-  std::cout << decodeVarint(enc).value_or(-1) << "\n";
+  std::cout << decodeVarint(enc).first.value_or(-1) << "\n";
 
   enc = encodeI64(164.25);
   std::cout << enc << "\n";
   std::cout << decodeI64(enc).value_or(-1) << "\n";
+
+  enc = encodeStr("testing");
+  std::cout << enc << "\n";
+  std::cout << decodeStr(enc).first.value_or("error") << "\n";
 }
